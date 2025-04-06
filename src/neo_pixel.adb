a00baf743df6780e0324208f8ce7a19d559ba034
Maxim Reznik <reznikmm@gmail.com>
2025-04-06 12:29:41 +0300

    Add a simple Neo Pixel generic package

--- src/neo_pixel.adb
+++ src/neo_pixel.adb
@@ -3,16 +3,7 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 ----------------------------------------------------------------
 
-with A0B.Callbacks.Generic_Subprogram;
-
-with Ada.Synchronous_Task_Control;
-with Interfaces;
-
 package body Neo_Pixel is
-
-   package Suspension_Objec_Callbacks is new A0B.Callbacks.Generic_Subprogram
-     (Ada.Synchronous_Task_Control.Suspension_Object,
-      Ada.Synchronous_Task_Control.Set_True);
 
    -----------------------
    -- Generic_LED_Strip --
@@ -20,116 +11,50 @@ package body Neo_Pixel is
 
    package body Generic_LED_Strip is
 
-      procedure Bit_Sent (Again : out Boolean);
+      Duty_0 : constant Pulse_Width :=
+        Pulse_Width (Chip.Pulse_Zero / (1E9 / Base_Frequency));
 
-      Period : constant Positive :=
-         PWN_Frequency / Configuration.Frequency;
+      Duty_1 : constant Pulse_Width :=
+        Pulse_Width (Chip.Pulse_One / (1E9 / Base_Frequency));
 
-      Duty_0 : constant Natural :=
-        Configuration.Pulse_Zero / (1E9 / PWN_Frequency);
+      ---------------
+      -- To_Pulses --
+      ---------------
 
-      Duty_1 : constant Natural :=
-        Configuration.Pulse_One / (1E9 / PWN_Frequency);
+      function To_Pulses (Pixel : Neo_Pixel.Pixel) return Pixel_Pulses is
 
-      Reset : constant Positive :=
-        Configuration.Reset / (1E9 / PWN_Frequency);
+         procedure Write
+           (Color  : Neo_Pixel.Color;
+            Result : out Pixel_Pulses;
+            Next   : in out Index);
 
-      Word        : Interfaces.Unsigned_32 := 0;
-      Bits_Left   : Natural range 0 .. 24 := 0;
-      Pixels_Left : Natural := 0 with Volatile;
-      Signal      : A0B.Callbacks.Callback;
-      Start_Lock  : Ada.Synchronous_Task_Control.Suspension_Object;
-      Send_Lock   : aliased Ada.Synchronous_Task_Control.Suspension_Object;
+         -----------
+         -- Write --
+         -----------
 
-      Bit_Callback : constant A0B.Callbacks.Callback :=
-        Suspension_Objec_Callbacks.Create_Callback (Send_Lock);
-
-      --------------
-      -- Bit_Sent --
-      --------------
-
-      procedure Bit_Sent (Again : out Boolean) is
-         use type Interfaces.Unsigned_32;
-         Bit : Boolean;
-      begin
-         if Bits_Left = 0 then
-            if Pixels_Left = 0 then
-               --  Send reset (no pulse for 50us) then call Done
-               Start_PWM
-                 (Device,
-                  Period => Reset,
-                  Duty   => 0,
-                  Done   => Signal);
-
-               Again := False;
-
-               return;
-
-            else
-
-               declare
-                  use Interfaces;
-                  V : Pixel;
-               begin
-                  Next_Pixel (Using, V);
-
-                  Word :=
-                    Shift_Left (Unsigned_32 (V.Green), 16)
-                      + Shift_Left (Unsigned_32 (V.Red), 8)
-                      + Unsigned_32 (V.Blue);
-
-                  Pixels_Left := Pixels_Left - 1;
-                  Bits_Left := 24;
-               end;
-            end if;
-         end if;
-
-         Bit := (Word and 16#80_00_00#) /= 0;
-         Word := Interfaces.Shift_Left (Word, 1);
-         Bits_Left := Bits_Left - 1;
-
-         Start_PWM
-           (Device,
-            Period => Period,
-            Duty   => (if Bit then Duty_1 else Duty_0),
-            Done   => Bit_Callback);
-
-         Again := True;
-      end Bit_Sent;
-
-      --------------------------
-      -- Start_Pixels_Sending --
-      --------------------------
-
-      procedure Start_Pixels_Sending
-        (Count : Positive;
-         Done  : A0B.Callbacks.Callback) is
-      begin
-         Pixels_Left := Count;
-         Signal := Done;
-         Ada.Synchronous_Task_Control.Set_True (Start_Lock);
-      end Start_Pixels_Sending;
-
-      task Sender;
-
-      ------------
-      -- Sender --
-      ------------
-
-      task body Sender is
-         Again : Boolean;
-      begin
-         loop
-            Ada.Synchronous_Task_Control.Suspend_Until_True (Start_Lock);
-            Bits_Left := 0;
-
-            loop
-               Bit_Sent (Again);
-               exit when not Again;
-               Ada.Synchronous_Task_Control.Suspend_Until_True (Send_Lock);
+         procedure Write
+           (Color  : Neo_Pixel.Color;
+            Result : out Pixel_Pulses;
+            Next   : in out Index)
+         is
+            Value : Neo_Pixel.Color := Color;
+         begin
+            for J in 1 .. 8 loop
+               Result (Next) :=
+                 (if (Value and 16#80#) = 0 then Duty_0 else Duty_1);
+               Value := 2 * Value;
+               Next := Next + 1;
             end loop;
-         end loop;
-      end Sender;
+         end Write;
+
+         Next : Index := 1;
+      begin
+         return Result : Pixel_Pulses do
+            Write (Pixel.Green, Result, Next);
+            Write (Pixel.Red, Result, Next);
+            Write (Pixel.Blue, Result, Next);
+         end return;
+      end To_Pulses;
 
    end Generic_LED_Strip;
 
--- src/neo_pixel.ads
+++ src/neo_pixel.ads
@@ -3,8 +3,10 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 ----------------------------------------------------------------
 
-with A0B.Callbacks;
---  with Interfaces;
+--  This package allows you to convert pixels from RGB to a bunch of PWM pulses
+--  in accordance with the chip specification. The you send this bunch to
+--  PWM timer (using DMA probably). To start a new sequence of pixels, pause
+--  with a value of zero for the time specified by Reset_Pulse.
 
 package Neo_Pixel is
 
@@ -16,46 +18,49 @@ package Neo_Pixel is
       Green : Color;
       Blue  : Color;
    end record;
+   --  Pixel encoded as RGB
 
    type Chip_Configuration is record
-      Frequency  : Positive;
-      Pulse_One  : Positive;  -- in ns
-      Pulse_Zero : Positive;  -- in ns
-      Reset      : Positive;  -- in ns
+      Frequency  : Positive;  --  in Hz
+      Pulse_One  : Positive;  --  in nano seconds
+      Pulse_Zero : Positive;  --  in nano seconds
+      Reset      : Positive;  --  in nano seconds
    end record;
    --  Configuration for a particular LED controller chip.
 
    generic
 
-      type PWM_Device (<>) is limited private;
+      type Pulse_Width is mod <>;
+      --  Pulse width representation
 
-      with procedure Start_PWM
-        (Self   : in out PWM_Device;
-         Period : Positive;
-         Duty   : Natural;
-         Done   : A0B.Callbacks.Callback);
-      --  Period and Duty in PWN_Frequency pulses
+      type Index is range <>;
+      --  An index in pulse width array
 
-      type Pixel_Buffer (<>) is limited private;
+      type Pulse_Width_Array is array (Index range <>) of Pulse_Width;
+      --  Array of pulse width
 
-      with procedure Next_Pixel
-        (Self  : in out Pixel_Buffer;
-         Value : out Pixel);
-      --  This should be fast, because it's called from IRQ, that should
-      --  react in 1.25us.
+      Base_Frequency : Positive;
+      --  The frequency that determines the unit of pulse width. For example
+      --  1MHz means Pulse_Width in microseconds.
 
-      Configuration : Chip_Configuration;      --  Chip configuration
-      Device        : in out PWM_Device;       --  PWM device
-      Using         : in out Pixel_Buffer;     --  Pixel source
-      PWN_Frequency : Positive := 20_000_000;  --  Timer frequency (50ns)
+      Chip : Chip_Configuration;
+      --  Chip configuration
 
    package Generic_LED_Strip is
 
-      procedure Start_Pixels_Sending
-        (Count : Positive;
-         Done  : A0B.Callbacks.Callback);
-      --  Start sending Count pixels and return immediately. Callback Done is
-      --  called  when all pixels and reset command are sent.
+      subtype Pixel_Pulses is Pulse_Width_Array (1 .. 24);
+      --  A pixel encoded as 24 pulses, 3 color for 8 pulses each
+
+      function To_Pulses (Pixel : Neo_Pixel.Pixel) return Pixel_Pulses;
+      --  Convert a pixel to a bunch of pulses
+
+      function Reset_Pulse return Pulse_Width;
+      --  How long reset pulse is
+
+   private
+
+      function Reset_Pulse return Pulse_Width is
+        (Pulse_Width (Chip.Reset / (1E9 /  Chip.Frequency)));
 
    end Generic_LED_Strip;
 
@@ -64,17 +69,20 @@ package Neo_Pixel is
       Pulse_Zero => 300,      --  0.3 us
       Pulse_One  => 600,      --  0.6 us
       Reset      => 80_000);  --  80us
+   --  Parameters for SK6812 chip
 
    WS2812 : constant Chip_Configuration :=
      (Frequency  => 800_000,  --  800 kHz (1.25 us)
       Pulse_Zero => 350,      --  0.35 us
       Pulse_One  => 700,      --  0.7 us
       Reset      => 50_000);  --  50us
+   --  Parameters for WS2812 chip
 
    WS2812B : constant Chip_Configuration :=
      (Frequency  => 800_000,  --  800 kHz (1.25 us)
       Pulse_Zero => 400,      --  0.4 us
       Pulse_One  => 800,      --  0.8 us
       Reset      => 50_000);  --  50us
+   --  Parameters for WS2812B chip
 
 end Neo_Pixel;
